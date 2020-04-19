870d7a191adcbd7e20605ea75d43bd46ee6a019e
Chameleon Cloud User <cc@magesh-cama.novalocal>
2020-04-19 00:32:45 +0000

    added wsgi.py file for deployment

--- app.py
+++ app.py
@@ -180,4 +180,4 @@ def peak_flow():
 
 
 if __name__ == '__main_':
-    app.run(debug=True, host='127.0.0.1', port=80)  # run app in debug mode on port 80
+    app.run(host='0.0.0.0')  # run app in debug mode on port 80
